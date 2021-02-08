3ca6aa5798397034a0406f8731fc76f0ae05953b
rtlikeone <info@wslm.nl>
2021-02-08 22:30:51 +0100

    [message here: push from shell edit]

--- projects/battleship-game/script.js
+++ projects/battleship-game/script.js
@@ -1,22 +1,47 @@
 "use-strict";
 
+const input_field = document.querySelector("#insert-target");
+const fire_button = document.querySelector("#fire-button");
+const grid = document.querySelector("#grid");
 const y_axis = [...Array(7).keys()];
 const x_axis = [];
 
+// Set values in x_axis
 for (let i of Array(7).keys()) {
-    x_axis.push(String.fromCharCode("A".charCodeAt(0) + i));
+    x_axis.push(String.fromCharCode("G".charCodeAt(0) - i));
 }
 
-console.log(y_axis);
-console.log(x_axis);
+// console.log(y_axis);
+// console.log(x_axis);
+// console.log(grid);
 
-// Create grid of divs
-const grid = document.querySelector("#grid");
-
-console.log(grid);
-
-for (let i = 0; i < 49; i++) {
-    let div = document.createElement("div");
-    div.className = `cell cell${i}`;
-    grid.appendChild(div);
+// Populate grid with divs
+for (let i in x_axis) {
+    for (let j of y_axis) {
+        let div = document.createElement("div");
+        div.className = `cell cell${x_axis[i]}`;
+        div.id = `${x_axis[i]}${j + 1}`;
+        grid.appendChild(div);
+    }
 }
+
+function showTarget() {
+    console.log(`Target: ${input_field.value}`);
+    input_field.value = "";
+}
+
+fire_button.addEventListener("click", showTarget);
+
+input_field.addEventListener("keydown", (e) => {
+    const divs = grid.childNodes;
+
+    if (e.key === "Enter") {
+        for (let i = 0; i < grid.childNodes.length; i++) {
+            if (divs[i].id == e.target.value) {
+                divs[i].style.backgroundColor = "red";
+            }
+        }
+        console.log(`Target: ${e.target.value}`);
+        e.target.value = "";
+    }
+});
